ae97d78c062ac829621d2ace37be7a04de844073
Jeffrey Macko <mackoj@users.noreply.github.com>
2024-04-16 15:41:49 +0200

    fix: use typesafe window groups instead of id alone (#8)

--- PlanetsViewer/Views/SolarSystemButton.swift
+++ PlanetsViewer/Views/SolarSystemButton.swift
@@ -18,9 +18,9 @@ struct SolarSystemButton: View {
             helper.selectedPlanet = planet
             Task {
                 if helper.isShowingSolar {
-                    dismissImmersiveSpace(id: "Globe")
+                  dismissImmersiveSpace(id: planet.id.rawValue, value: planet)
                 } else {
-                    openImmersiveSpace(id: "Globe")
+                    openImmersiveSpace(id: planet.id.rawValue, value: planet)
                 }
             }
         } label: {
